a4d7850e00381c0336f763be6fbd1abb2a421a50
murraymatias <murray.matias@gmail.com>
2020-05-26 21:53:39 -0300

    commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,11 @@
 import Anuncio_Auto from "./clases.js";
 
+let listaAnuncios;
+
+window.addEventListener('load', function () {
+    traerAnuncios();
+    generarTabla();
+});
 
 document.getElementById("btnGuardar").addEventListener("click", function(e) {
     e.preventDefault();
@@ -32,4 +38,34 @@ function enviarAnuncio(anuncioAuto) {
   xhr.open("POST", "alta", true);
   xhr.setRequestHeader("Content-Type", "Application/JSON");
   xhr.send(JSON.stringify(anuncioAuto));
+}
+
+function generarTabla() {
+    console.log(listaAnuncios)
+    let tabla = document.getElementById("tablaAnuncios");
+
+    for (anuncioAuto of listaAnuncios) {
+      let fila = document.createElement("tr");
+      for (dato in anuncioAuto) {
+        let celda = document.createElement("td");
+        //celda.addEventListener('click', cargarFormulario);      
+        let texto = document.createTextNode(anuncioAuto[dato]);
+        celda.appendChild(texto);
+        fila.appendChild(celda);  
+        
+      }
+      tabla.appendChild(fila);
+    }
+}
+
+function traerAnuncios() {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+         listaAnuncios = JSON.parse(xhr.responseText);     
+
+      }
+    };
+    xhr.open("GET", "traer", true);
+    xhr.send();
 }
