0bb86ba0241a736412090c592e9d0790b8406899
murraymatias <murray.matias@gmail.com>
2020-07-31 20:37:25 -0300

    abm en localstoraje

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,10 @@
 import Anuncio_Auto from "./clases.js";
 
 let listaAnuncios;
+let anuncioSeleccionado = '';
+
+$('#btnLimpiar').hide();
+$('#btnBorrar').hide();
 
 window.addEventListener("load", function () {
   traerAnuncios();
@@ -9,16 +13,50 @@ window.addEventListener("load", function () {
 document.getElementById("btnGuardar").addEventListener("click", function (e) {
   e.preventDefault();
   guardarAnuncio();
+  actualizarTabla();
 });
 
-document.getElementById("selFiltroTransaccion").addEventListener("change", function () {
-  if (document.getElementById("selFiltroTransaccion").value == "na") {
-    generarTabla(listaAnuncios);
-  } else {
-    let listaFiltrada = listaAnuncios.filter((anuncio) => anuncio.transaccion == document.getElementById("selFiltroTransaccion").value);
-    generarTabla(listaFiltrada);
-  }
-  });
+document.getElementById("btnLimpiar").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("txtTitulo").value = '';
+  document.getElementById("selTransaccion").value = '';
+  document.getElementById("txtDescipcion").value = '';
+  document.getElementById("txtPrecio").value = '';
+  document.getElementById("txtPuertas").value = '';
+  document.getElementById("txtKms").value = '';
+  document.getElementById("txtPotencia").value = '';
+
+  anuncioSeleccionado = '';
+
+  $('#btnLimpiar').hide();
+  $('#btnBorrar').hide();
+});
+
+document.getElementById("btnBorrar").addEventListener("click", function (e) {
+  e.preventDefault();
+  document.getElementById("txtTitulo").value = '';
+  document.getElementById("selTransaccion").value = '';
+  document.getElementById("txtDescipcion").value = '';
+  document.getElementById("txtPrecio").value = '';
+  document.getElementById("txtPuertas").value = '';
+  document.getElementById("txtKms").value = '';
+  document.getElementById("txtPotencia").value = '';
+
+  deleteAnuncio();
+
+  $('#btnLimpiar').hide();
+  $('#btnBorrar').hide();
+});
+
+document.getElementById("selFiltroTransaccion").addEventListener("change", actualizarTabla);
+document.getElementById('checkTitulo').addEventListener("change", actualizarTabla);
+document.getElementById('checkTransaccion').addEventListener("change", actualizarTabla);
+document.getElementById('checkDescripccion').addEventListener("change", actualizarTabla);
+document.getElementById('checkDescripccion').addEventListener("change", actualizarTabla);
+document.getElementById('checkPrecio').addEventListener("change", actualizarTabla);
+document.getElementById('checkPuertas').addEventListener("change", actualizarTabla);
+document.getElementById('checkKms').addEventListener("change", actualizarTabla);
+document.getElementById('checkPotencia').addEventListener("change", actualizarTabla);
 
 function guardarAnuncio() {
   let anuncioAuto = new Anuncio_Auto(
@@ -30,12 +68,27 @@ function guardarAnuncio() {
     document.getElementById("txtKms").value,
     document.getElementById("txtPotencia").value
   );
+  document.getElementById("txtTitulo").value='';
+  document.getElementById("selTransaccion").value='';
+  document.getElementById("txtDescipcion").value='';
+  document.getElementById("txtPrecio").value='';
+  document.getElementById("txtPuertas").value='';
+  document.getElementById("txtKms").value='';
+  document.getElementById("txtPotencia").value='';
 
-  enviarAnuncio(anuncioAuto);
+  if(anuncioSeleccionado == '')
+  {
+    enviarAnuncio(anuncioAuto);
+  }
+  else
+  {
+    updateAnuncio(anuncioAuto);
+  }
 }
 
-function generarTabla(lista) {
+function generarTabla(list) {
 
+  let lista = filtroMap(list);
   let tabla = document.getElementById("tablaAnuncios");  
   tabla.innerHTML = '';  
   let head = document.createElement("thead");
@@ -94,6 +147,8 @@ function calcularPromedio(lista)
 function cargarFormulario()
 {
   let anuncio = listaAnuncios.find(elemento => elemento.id == this.parentElement.firstChild.innerText);
+  console.log(anuncio);
+  anuncioSeleccionado=anuncio;
 
   document.getElementById("txtTitulo").value = anuncio.titulo;
   document.getElementById("selTransaccion").value = anuncio.transaccion;
@@ -102,61 +157,113 @@ function cargarFormulario()
   document.getElementById("txtPuertas").value = anuncio.puertas;
   document.getElementById("txtKms").value = anuncio.kms;
   document.getElementById("txtPotencia").value = anuncio.potencia;
+
+  $('#btnLimpiar').show();
+  $('#btnBorrar').show();
+  
 }
 
-function filtroMap(lista)
-{
-
+function filtroMap(lista){
+return lista.map(function(anuncio){
+    let anuncioMapeado = new Object()
+    anuncioMapeado.id = anuncio.id;
+    if(document.getElementById('checkTitulo').checked)
+    {
+      anuncioMapeado.titulo = anuncio.titulo;
+    }
+    if(document.getElementById('checkTransaccion').checked)
+    {
+      anuncioMapeado.transaccion = anuncio.transaccion;
+    }
+    if(document.getElementById('checkDescripccion').checked)
+    {
+      anuncioMapeado.descripcion = anuncio.descripcion;
+    }
+    if(document.getElementById('checkDescripccion').checked)
+    {
+      anuncioMapeado.descripcion = anuncio.descripcion;
+    }
+    if(document.getElementById('checkPrecio').checked)
+    {
+      anuncioMapeado.precio = anuncio.precio;
+    }
+    if(document.getElementById('checkPuertas').checked)
+    {
+      anuncioMapeado.puertas = anuncio.puertas;
+    }
+    if(document.getElementById('checkKms').checked)
+    {
+      anuncioMapeado.kms = anuncio.kms;
+    }
+    if(document.getElementById('checkPotencia').checked)
+    {
+      anuncioMapeado.potencia = anuncio.potencia;
+    }
+    return anuncioMapeado;
+  });
 }
 
 //GET Anuncios
-function traerAnuncios() {
-  let xhr = new XMLHttpRequest();
+function traerAnuncios(){
+  //spinner
+  $('body').toggleClass('loading');
+  setTimeout(()=>{$('body').toggleClass('loading');},2000);
 
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      listaAnuncios = JSON.parse(xhr.responseText)["data"];
-      generarTabla(listaAnuncios);
-      $("body").toggleClass("loading");
-    }
-  };
-  xhr.open("GET", "traer", true);
-  xhr.send();
-  $("body").toggleClass("loading");
+
+  listaAnuncios = JSON.parse(localStorage.getItem('anuncios'));
+  console.log(listaAnuncios);
+  generarTabla(listaAnuncios);
 }
 
 //ADD anuncio
-function enviarAnuncio(anuncioAuto) {
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      let per = JSON.parse(xhr.responseText);
-      document.getElementById("formulario").reset();
-      $("body").toggleClass("loading");
+function enviarAnuncio(anuncioAuto){
+  //spinner
+  $('body').toggleClass('loading');
+  setTimeout(()=>{$('body').toggleClass('loading');},2000);
 
-      traerAnuncios();
-    }
-  };
-  $("body").toggleClass("loading");
-  xhr.open("POST", "alta", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(anuncioAuto));
+
+  let ultimoId = listaAnuncios.reduce((acc,obj)=>{
+    if(obj.id>acc){acc=obj.id;}return acc;
+  },0);
+
+  anuncioAuto.id=ultimoId+1;
+  listaAnuncios.push(anuncioAuto);
+  localStorage.setItem('anuncios',JSON.stringify(listaAnuncios));
 }
 
-function updateAnuncio(anuncioAuto)
-{
-  let xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      let per = JSON.parse(xhr.responseText);
-      document.getElementById("formulario").reset();
-      $("body").toggleClass("loading");
+function updateAnuncio(anuncioAuto){
+  //spinner
+  $('body').toggleClass('loading');
+  setTimeout(()=>{$('body').toggleClass('loading');},2000);
 
-      traerAnuncios();
-    }
-  };
-  $("body").toggleClass("loading");
-  xhr.open("POST", "modificar", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(anuncioAuto));
+  anuncioSeleccionado.titulo = anuncioAuto.titulo;
+  anuncioSeleccionado.transaccion = anuncioAuto.transaccion;
+  anuncioSeleccionado.descripcion = anuncioAuto.descripcion;
+  anuncioSeleccionado.precio = anuncioAuto.precio;
+  anuncioSeleccionado.puertas = anuncioAuto.puertas;
+  anuncioSeleccionado.kms = anuncioAuto.kms;
+  anuncioSeleccionado.potencia = anuncioAuto.potencia;
+
+  localStorage.setItem('anuncios',JSON.stringify(listaAnuncios));
+}
+
+function deleteAnuncio()
+{
+  //spinner
+  $('body').toggleClass('loading');
+  setTimeout(()=>{$('body').toggleClass('loading');},2000);
+  
+  listaAnuncios.pop(anuncioSeleccionado);
+
+  localStorage.setItem('anuncios',JSON.stringify(listaAnuncios));
+}
+
+function actualizarTabla()
+{
+  if (document.getElementById("selFiltroTransaccion").value == "na") {
+    generarTabla(listaAnuncios);
+  } else {
+    let listaFiltrada = listaAnuncios.filter((anuncio) => anuncio.transaccion == document.getElementById("selFiltroTransaccion").value);
+    generarTabla(listaFiltrada);
+  }
 }
